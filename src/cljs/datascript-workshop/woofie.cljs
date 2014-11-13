3efc5abafe272e994121a4ad1f116595a1aee495
Brice Fernandes <brice.fernandes@gmail.com>
2014-11-13 13:59:53 +0000

    Add first Reagent Atom

--- src/cljs/datascript-workshop/woofie.cljs
+++ src/cljs/datascript-workshop/woofie.cljs
@@ -1,9 +1,19 @@
 (ns datascript-workshop.woofie
-  (:require [reagent.core :as reagent]))
+  (:require [reagent.core :as reagent :refer [atom]]))
+
+(def app-state (atom {:number-users 0}))
+
+(defn new-user! []
+  (swap! app-state #(update-in %1 [:number-users] inc)))
 
 (defn woofie []
    [:div
-    [:header [:h1 "Welcome to Woofie"]]
-    [:p "The dog social network"]])
+    [:header [:h1 "Welcome to Woofie"] [:p "The dog social network"]]
+
+    [:div {:class "site-info"}
+     [:p (str "Number of dogs on site: " (:number-users @app-state))]]
+
+    [:div {:class "user-tools"}
+     [:button {:on-click (new-user!)} "Register"]]])
 
 (reagent/render-component [woofie] (.-body js/document))
