23afde1f3ccd844faa770b013a20d2cbf7771f86
Brice Fernandes <brice.fernandes@gmail.com>
2014-11-14 00:28:29 +0000

    Correct glaring mistakes and add solution

--- src/cljs/datascript-workshop/woofie.cljs
+++ src/cljs/datascript-workshop/woofie.cljs
@@ -26,7 +26,7 @@
 (def q-dog-names '[:find ?n :where [?e :name ?n]])
 
 (def q-pairings-purebreed
-  '[:find ?m ?f
+  '[:find ?m ?f ?e ?a
     :where [?e :name ?m]
            [?e :sex "m"]
            [?e :breed ?b]
@@ -36,25 +36,28 @@
           ])
 
 (def q-pairings-all
-  '[:find ?m ?f
+  '[:find ?m ?f ?e ?a
     :where [?e :name ?m]
            [?e :sex "m"]
            [?a :name ?f]
            [?a :sex "f"]
           ])
 
+;; Finding every dogs
+(def q-unique-dogs '[:find ?n ?e :where [?e :name ?n]])
 
 (comment
   (d/q q-dog-names @conn)
   (d/q q-pairings-purebreed @conn)
   (d/q q-pairings-all @conn)
+  (d/q q-unique-dogs @conn)
   )
 
 
 (def app-state (atom {:number-users 0}))
 
 (defn new-user! []
-  (swap! app-state #(update-in %1 [:number-users] inc)))
+  (swap! app-state update-in [:number-users] inc))
 
 (defn add-betty! []
   (d/transact! conn [{:name "Betty"
@@ -64,31 +67,32 @@
                       :owner "George Orwell"}]))
 
 (defn woofie []
-  (let [dogs (bind conn q-dog-names)]
+  (let [matches (bind conn q-pairings-purebreed)
+        members (bind conn q-unique-dogs)]
    [:div
     [:header [:h1 "Welcome to Woofie"] [:p "The dog social network"]]
 
     [:div {:class "site-info"}
-     [:p (str "Number of dogs on site: " (:number-users @app-state))]]
+     [:p (str "Number of dogs on site: " (count @members))]]
 
     [:div {:class "user-tools"}
-     [:button {:on-click #(new-user!)} "Register"]
-     [:button {:on-click #(add-betty!)} "Add betty"]
+     [:button {:on-click add-betty!} "Add betty"]
      ]
 
+    ;; Listing all the dogs
     [:div {:class "members"}
      [:h3 "Members"]
      [:ul
       (map
        (fn [n] [:li (str (n 0))])
-       @dogs)]]
+       @members))]]
 
     [:div {:class "matches"}
      [:h3 "Matches"]
      [:ul
       (map
        (fn [p] [:li (str (p 0) " and " (p 1))])
-       (d/q q-pairings-purebreed @conn))]]
+       @matches)]]
 
     ]))
 
